61ac046db22b3d813a010ba948d950b48c090115
Agatha <agatha_simao19@hotmail.com>
2025-05-22 01:36:08 -0300

    your_tts voice clone

--- test/coquitts/xtts.py
+++ test/coquitts/xtts.py
@@ -5,7 +5,6 @@ from TTS.api import TTS
 import sounddevice as sd
 import soundfile as sf
 
-
 text = "It took me quite a long time to develop a voice, and now that I have it I'm not going to be silent."
 print(TTS().list_models())
 
@@ -23,13 +22,16 @@ tts.tts_to_file(text=text,
                 file_path="output.wav",
                 speaker=tts.speakers[0],
                 language="en",
-                split_sentences=True
+                split_sentences=True,
                 )
 
 end = time.time()
+
+
 print("time: " + str(end - start))
 
 data, samplerate = sf.read("output.wav")
+print(f"Sample rate: {samplerate}, Data shape: {data.shape}")
 
 sd.play(data, samplerate)
 sd.wait()
